0bc7b016478120c912a16d1251dbcc60a012203c
Koutaroo <sss5555656sksd555656ktrktr@gmail.com>
2020-08-06 20:51:30 +0900

    Initial Commit

---
+++ scProject01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  scProject01
+//
+//  Created by 坂田光太郎 on 2020/08/06.
+//  Copyright © 2020 sc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
